c1f57967c29a0d83ec0ad68131a5071604f53304
sp1r14ual <sp1r14ual@gmail.com>
2023-04-09 15:35:40 +0700

    fix: removed unnecessary comments

--- script.js
+++ script.js
@@ -187,23 +187,12 @@ btnLogin.addEventListener('click', function (e) {
       day: 'numeric',
       month: 'numeric',
       year: 'numeric',
-      // weekday: 'long',
     };
-    // const locale = navigator.language;
-    // console.log(locale);
 
     labelDate.textContent = new Intl.DateTimeFormat(
       currentAccount.locale,
       options
     ).format(now);
-
-    // const now = new Date();
-    // const day = `${now.getDate()}`.padStart(2, '0');
-    // const month = `${now.getMonth() + 1}`.padStart(2, 0);
-    // const year = now.getFullYear();
-    // const hour = `${now.getHours()}`.padStart(2, '0');
-    // const min = `${now.getMinutes()}`.padStart(2, '0');
-    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
 
     //Clear input fields
     inputLoginPin.value = inputLoginUsername.value = '';
